9cbe2c13511cc778261817aca21e55979bb01a1e
Vitaliy <Groteckon@mail.ru>
2023-04-23 18:21:34 +0300

    changed words

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int cost_ticket = 12_753;
-        int M1 = 20; // "М1" обозначает 1 милю
+        int costTicket = 12_753;
+        int bonus = 20; // За 1 милю
 
-        System.out.println("Количество начисленных милей =" + cost_ticket / M1);
+        System.out.println("Количество начисленных милей =" + costTicket / bonus);
 
     }
 }
